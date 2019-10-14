e822fb08749cbe89b937c23e73b8a59d1a6e601a
VasyaBilak <vasyabilak.vb@gmail.com>
2019-10-14 18:21:45 +0300

    add style

--- js/my.js
+++ js/my.js
@@ -1,12 +1,30 @@
 'use strict';
 
-function createElement(el, style, text) {
-    let newLi = document.createElement(`${el}`);
+function createElement(el, style, ...text) {
+    let result = document.createElement(`${el}`);
 
-    newLi.innerHTML = `${text}`;
-    
-    newLi.setAttribute("style", `${style}`);
-    return newLi;
+    // let text = arr.forEach(function(element) {
+    //   return element;
+    // });
+
+    result.innerHTML = `${text}`;
+
+    let styleValue = JSON.stringify(style.style)
+                      .replace('{', '')
+                      .replace('}', '')
+                      .replace('"', '')
+                      .replace('"', '')
+                      .replace('"', '')
+                      .replace('"', '');
+
+    let regex = /[A-Z]/g;
+    let index = styleValue.match(regex);
+    index = styleValue.indexOf(index);
+
+    styleValue = styleValue.slice(0, index) + '-' + styleValue.slice(index, styleValue.length);
+
+    result.setAttribute("style", `${styleValue}`);
+    return result;
 }
 
 function render(fn, id) {
@@ -18,11 +36,21 @@ const React = {
     render
   };
 
-const app = React.createElement(
-    'div',
-    "background-color: red;",
-    'Hello',
-  );
+// const app = React.createElement(
+//     'div',
+//     "background-color: red;",
+//     'Hello',
+//   );
 
+const app = React.createElement(
+  'div',
+  { style: { backgroundColor: 'red' } },
+  // [
+  //   React.createElement('span', undefined, 'Hello world'),
+  //   React.createElement('br'),
+  //   'This is just a text node',
+  //   React.createElement('div', { textContent: 'Text content' }),
+  // ],
+);
 
 React.render(app, document.getElementById('app'));
